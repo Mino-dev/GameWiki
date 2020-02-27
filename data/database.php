20a28c81f304a962c34cbca3bf7cf12daa42494e
Mino-tan <j.vinceeleazar@gmail.com>
2020-02-27 23:13:02 +0800

    Commit-4
    Updates to the database, added registration (work in progress) in the project. HELP MEEEEE

--- data/database.php
+++ data/database.php
@@ -1,43 +1,61 @@
 <?php
-    $connection;
-    $result;
+    global $connection;
     function connectDB(){
         $db_host = "localhost";
-        $db_username = "id12624664_gamewiki";
-        $db_password = "phpfinals";
-        $db_name = "id12624664_gamewiki";
+        //$db_username = "id12624664_gamewiki";
+        //$db_password = "phpfinals";
+        //$db_name = "id12624664_gamewiki";
+        $db_username = "root";
+		$db_password = "";
+        $db_name = "wiki";
+
+        global $connection;
         $connection = mysqli_connect($db_host,
 									 $db_username,
 									 $db_password,
                                      $db_name);
-        
         if(mysqli_connect_errno()!=0){
             return false;
         }else{
             return true;
         }
     }
-    function isUsernameExisiting($username, $password){
-        $sql = "SELECT * FROM users
-        WHERE username = '$username'
-        AND password = '$password'";
+    
+    function isEmailExisting($email){
+        $sql = "SELECT uemail FROM users
+                WHERE uemail = '$email'";
+        
+        global $connection;
+        $result = mysqli_query($connection, $sql);
+        if(mysqli_num_rows($result) == 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 
+    function isUsernameExisting($username){
+        $sql = "SELECT username FROM users
+                WHERE username = '$username'";
+        global $connection;
         $result = mysqli_query($connection, $sql);
 
-        if(mysqli_num_rows($result) != 0){
-            return true;
-        }else{
+        if(mysqli_num_rows($result) == 0){
             return false;
+        }else{
+            return true;
         }
     }
     function checkIfUniqueUsername($username, $uid){
-        $sql = "SELECT username FROM users
+        $sql = "SELECT username, upassword FROM users
                 WHERE uid = '$uid'
                 AND username = '$username'";
-        if(mysqli_num_rows($result) != 0){
-            return true;
-        }else{
+
+        $result = mysqli_query($connection, $sql);
+        if(mysqli_num_rows($result) == 0){
             return false;
+        }else{
+            return true;
         }
     }
     function updateUsername($username, $uid){
@@ -51,14 +69,25 @@
             return false;
         }
     }
-    function retrieveUserData(){
+    function registerUser($username, $password, $email, $type = 1, $pfp='../img/default.png'){
+
+        $sql = "INSERT INTO users(username, upassword, uemail, utype, upfp)
+                VALUES('$username', '$password', '$email', '$type', '$pfp')";
+        global $connection;
+        $result = mysqli_query($connection, $sql);
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function loginUser($username, $password){
         $ret = mysqli_fetch_array($result);
         $client = array("uid" => $row['uid'],
                     "username" => $row['username'],
                     "uemail" => $row['uemail'],
                     "utype" => $row['utype'],
                     "upfp"  => $row['upfp']);
-        return $client;
     }
     
 ?>
--- route/register-page.php
+++ route/register-page.php
@@ -1,6 +1,41 @@
+<?php
+    
+    require_once('data/database.php');
+        
+    connectDB();
+    if(isset($_POST['register'])){
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $cpassword = $_POST['cpassword'];
+
+        echo $email . "<br>" .$username . "<br>" . $password . "<br>" . $cpassword;
+        if(!isUsernameExisting($username)){
+            if(!isEmailExisting($email)){
+                if($cpassword!=$password){
+                    if(registerUser($username, $password, $email)){
+                        echo"<script>windows.location = 'login.php';</script>";
+                    }else{
+                        echo "registration oops";
+                    }
+                }else{
+                    echo "password doest not match oops";
+                }
+            }else{
+                echo "email oops";
+            }
+        }else{
+            echo "username oops";
+        }
+
+        
+    }
+?>
+
 <!--Navigation Bar -->
 <header class="lr:header-section header-section">
     <?php
+        
         include('template/navbar.html');
     ?>
 </header>
@@ -11,26 +46,26 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="register-email">Email</label>
-                <input type="email" class="form-control" id="register-email" placeholder="Email">
+                <input type="email" name="email" class="form-control" id="register-email" placeholder="Email" required>
             </div>
             <div class="form-group col-md-6">
                 <label for="register-username">Username</label>
-                <input type="text" class="form-control" id="register-username" placeholder="Username">
+                <input type="text" name="username" class="form-control" id="register-username" placeholder="Username" required>
             </div>
         </div>
         <div class="form-group row">
             <label for="register-password" class="col-sm-2 col-form-label">Password</label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" id="register-password" placeholder="Password">
+                <input type="password" name="password" class="form-control" id="register-password" placeholder="Password" required>
             </div>
         </div>
         <div class="form-group row">
             <label for="register-con-password" class="col-sm-2 col-form-label">Confirm Password</label>
             <div class="col-sm-10">
-                <input type="password" class="form-control" id="register-con-password" placeholder="Confirm Password">
+                <input type="password" name="cpassword" class="form-control" id="register-con-password" placeholder="Confirm Password" required>
             </div>
         </div>
-        <button type="submit" name="login" class="btn btn-primary">Sign In</button>
+        <button type="submit" name="register" class="btn btn-primary">Sign Up</button>
     </form>
 </section>
 <!--End of Login Section -->
