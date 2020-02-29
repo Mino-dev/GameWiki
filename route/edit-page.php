1a467835442ae3f82267e0067031c197fe17dac2
Mino-tan <j.vinceeleazar@gmail.com>
2020-03-01 00:07:22 +0800

    Commit-7
    Big changes in database and editing page. Need to add img edit for the page to actually be considered finished. Will proceed to functionalities tomorrow.

--- route/edit-page.php
+++ route/edit-page.php
@@ -1,6 +1,7 @@
 
 <?php
     session_start();
+
     if(isset($_SESSION['id']) && isset($_SESSION['log'])){
         $client = $_SESSION['client'];
         $email = $client['uemail'];
@@ -10,10 +11,103 @@
         $username = "";
     }
     if(isset($_POST['edit'])){
-        $tusername = $_POST['username'];
+        require_once('data/database.php'); 
+        if(connectDB()){
+            if(checkPassword($_POST['password'], $client['uid'])){
+                if($_POST['email'] != $client['uemail']){
+                    if(handleUniqueEmail($_POST['email'], $client['uid'])){
+                        if($_POST['username'] != $client['username']){
+                            if(handleUniqueUsername($_POST['username'],$client['uid'])){
+                                if(isset($_POST['chpassword']) && isset($_POST['cpassword']) && !empty($_POST['chpassword'])){
+                                    if($_POST['chpassword'] == $_POST['cpassword']){
+                                        if(updateUser($_POST['username'],$_POST['cpassword'],$_POST['email'],$client['uid'])){
+                                            retrieveUser();
+                                        }
+                                    }else{
+                                        echo "oops passwords";
+                                    }
+                                }else{
+                                    if(updateUser($_POST['username'],$_POST['password'],$_POST['email'],$client['uid'])){
+                                        retrieveUser();
+                                    }
+                                }
+                            }else{
+                                echo "oops username already taken";
+                            }
+                        }else{
+                            if(isset($_POST['chpassword']) && isset($_POST['cpassword'])&& !empty($_POST['chpassword'])){
+                                if($_POST['chpassword'] == $_POST['cpassword']){
+                                    if(updateUser($_POST['username'],$_POST['cpassword'],$_POST['email'],$client['oid'])){
+                                        retrieveUser();
+                                    }
+                                }else{
+                                    echo "oops passwords";
+                                }
+                            }
+                        }
+                    }else{
+                        echo "oops email already taken";
+                    }
+                }else{
+                    if($_POST['username'] != $client['username']){
+                        if(handleUniqueUsername($_POST['username'],$client['uid'])){
+                            if(isset($_POST['chpassword']) && isset($_POST['cpassword']) && !empty($_POST['chpassword'])){
+                                if($_POST['chpassword'] == $_POST['cpassword']){
+                                    if(updateUser($_POST['username'],$_POST['cpassword'],$_POST['email'],$client['uid'])){
+                                        retrieveUser();
+                                    }
+                                }else{
+                                    echo "oops passwords";
+                                }
+                            }else{
+                                if(updateUser($_POST['username'],$_POST['password'],$_POST['email'],$client['uid'])){
+                                    retrieveUser();
+                                }
+                            }
+                        }else{
+                            echo "oops username already taken";
+                        }
+                    }else{
+                        if(isset($_POST['chpassword']) && isset($_POST['cpassword'])&& !empty($_POST['chpassword'])){
+                            if($_POST['chpassword'] == $_POST['cpassword']){
+                                if(updateUser($_POST['username'],$_POST['cpassword'],$_POST['email'],$client['uid'])){
+                                    retrieveUser();
+                                }
+                            }else{
+                                echo "oops passwords";
+                            }
+                        }else{
+                            if(updateUser($_POST['username'],$_POST['password'],$_POST['email'],$client['uid'])){
+                                retrieveUser();
+                            }
+                        }
+                    }
+                }
+                
+            }else{
+                echo "oops password";
+            }
+            closeDB();
+        }
+    }
+    
+    function handleUniqueEmail($email, $uid){
+        if(checkIfUniqueEmail($email, $uid)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+ 
+    function handleUniqueUsername($username, $uid){
+        if(checkIfUniqueUsername($username, $uid)){
+            return true;                 
+        }
+        else{
+            return false;
+        }
     }
 ?>
-
 <header class="e:header-section header-section">
     <?php
         
@@ -36,15 +130,21 @@
             </div>
         </div>
         <div class="form-group row">
-            <label for="register-password" class="col-sm-2 col-form-label">Password</label>
+            <label for="register-password" class="col-sm-2 col-form-label">Change Password</label>
             <div class="col-sm-10">
-                <input type="password" name="password" class="form-control" id="register-password" placeholder="Password">
+                <input type="password" name="chpassword" class="form-control" id="register-new-password" placeholder="New Password">
             </div>
         </div>
         <div class="form-group row">
             <label for="register-con-password" class="col-sm-2 col-form-label">Confirm Password</label>
             <div class="col-sm-10">
-                <input type="password" name="cpassword" class="form-control" id="register-con-password" placeholder="Confirm Password">
+                <input type="password" name="cpassword" class="form-control" id="register-connew-password" placeholder="Confirm New Password">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="register-con-password" class="col-sm-2 col-form-label">Confirm Changes</label>
+            <div class="col-sm-10">
+                <input type="password" name="password" class="form-control" id="register-con-password" placeholder="Current Password" required>
             </div>
         </div>
         <button type="submit" name="edit" class="btn btn-primary">Edit</button>
