4bf070f5b4b2d5957be06e6234fc99f685c10fec
Mino-tan <j.vinceeleazar@gmail.com>
2020-03-03 16:46:57 +0800

    Commit-10

--- index.php
+++ index.php
@@ -10,15 +10,17 @@
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="style/style.css" type="text/css">
 		<title>Wiki</title>
 	</head>
 	<body>
+	
 		<div class="container-fluid">
 			<?php
 				include ('route/home-page.php');
 			?>
-    	</div>
+			
+		</div>		
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
